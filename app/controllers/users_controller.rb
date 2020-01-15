a5e4042f9bef02c427516c3d478f984b9081cf51
shekhar-patil <patilshekhar900@gmail.com>
2020-01-15 09:25:34 +0530

    add edit and update functionality in user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to alpha blog #{@user.name}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if(@user.update(user_params))
+      flash[:success] = 'Account updated successfully.'
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
 
   root 'page#home'
   get 'about', to: 'page#about'
+
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
