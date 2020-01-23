db2b1b2b8c0a353821b45283ad74609a1c4c1ccf
shekhar-patil <patilshekhar900@gmail.com>
2020-01-23 18:44:19 +0530

    navigation bar changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   root 'page#home'
   get 'about', to: 'page#about'
 
@@ -7,4 +6,8 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
